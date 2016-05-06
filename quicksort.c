185ad06a7f7195a1feed856b581adabe7dab801b
qianfeng <807132689@qq.com>
2016-05-06 17:26:30 +0800

    快速排序

--- quicksort.c
+++ quicksort.c
@@ -9,72 +9,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int QuickSort(int* str,int left,int right)
+void swap(int array[],int i,int j)
 {
-    if(left < right)
-    {
-        int pivotpos = Partition(str,left,right);
-        QuickSort(str, left, pivotpos - 1);
-        QuickSort(str, pivotpos+1, right);
-    }
-    return 0;
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
 
-int Partition(int* str, int low, int high)
+int partition(int array[],int low,int high)
 {
-    int c,pivot = str[low];
-    int i = low;
-    int j = high;
-    while (i != j)
+    int pv = array[low];
+    
+    while(low<high)
     {
-        while(i<j && str[i] < pivot) i++;
-        if(i<j) str[j--] = str[i];
-        while(i<j && pivot < str[j]) j--;
-        if(i<j) str[i++] = str[j];
-    }
-    str[i] = pivot;
-//    while(i<j)
-//    {
-//        while(i<j && pivot < str[j]) j--;
-//        while(i<j && str[i] < pivot) i++;
-//        if(i<j)
-//        {
-//            c = str[i];
-//            str[i] = str[j];
-//            str[j] = c;
-//            i++;
-//            j--;
-//        }
-//    }
-//    str[low] = str[i];
-//    str[i] = pivot;
-    return i;
-}
-
-int main(int argc, const char * argv[])
-{
-    int sort[100] = {};
-    int counter = 0;
-    printf("请输入序列:");
-    for(int i=0;i<100;i++)
-    {
-        scanf("%d",&sort[i]);
-        counter++;
-        if(getchar() == '\n')
+        while((low<high)&&(array[high]>=pv))
         {
-            break;
+            high--;
         }
+        swap(array,low,high);
+        
+        while((low<high)&&(array[low]<=pv))
+        {
+            low++;
+        }
+        swap(array,high,low);
     }
     
-    printf("%d \n",counter);
-    QuickSort(sort, 0, counter);
-    
-    
-    for(int i=0;i<counter;i++)
-    {
-        printf("%d ",sort[i]);
-    }
-    
-    printf("\n");
-    return 0;
+    //array[low] = pv;
+    return low;
 }
+
+void quickSort(int array[],int low,int high)
+{
+    if(low<high)
+    {
+        int pivot = partition(array, low, high);
+        QSort(array,low,pivot-1);
+        QSort(array, pivot+1, high);
+    }
+}
+
